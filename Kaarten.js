825cd5262bd4cec1ae75fdbe30a7810895724533
DanielGMelgarejo <145459680+DanielGMelgarejo@users.noreply.github.com>
2023-12-02 15:37:44 +0100

    update 2/12

--- Kaarten.js
+++ Kaarten.js
@@ -1,19 +1,19 @@
-/* In dit script plaats je de code om de kaart in de id Veemarkt te tekenen, zodat de AP-Hogeschool met adres Ellermanstraat 33 gecentreerd staat.
-Gebruik hiervoor de documentatie op https://leafletjs.com/ */
+/* In dit script plaats je de code om de kaart in de id apMap te tekenen, zodat de AP-Hogeschool met adres Ellermanstraat 33 gecentreerd staat.  
+Gebruik hiervoor de documentatie op https://leafletjs.com/ 
+*/
 
-let VeemarktMap = L.map('Veemarkt').setView([51.23009, 4.41616], 17);
+let map = L.map('Veemarkt').setView([51.23009, 4.41616], 17);
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-}).addTo(VeemarktMap);
+}).addTo(map);
+
 
 // bepaal de rechthoek rondom het gebouw van AP
 let bounds = [[51.23041, 4.4155], [51.22991, 4.41675]];
-
 // kleur de rechthoek in met de rode AP-kleur
-L.rectangle(bounds, {color: "#e60005", weight: 1}).addTo(VeemarktMap);
-
+L.rectangle(bounds, {color: "#e60005", weight: 1}).addTo(map);
 // plaats een marker met als tekst "AP-Hogeschool" en eronder "Ellermanstraat 33"
-let apMarker = L.marker([51.23009, 4.41616]).addTo(VeemarktMap);
+let apMarker = L.marker([51.23009, 4.41616]).addTo(map);
 apMarker.bindPopup("<b>AP-Hogeschool</b><br>Ellermanstraat 33").openPopup();
