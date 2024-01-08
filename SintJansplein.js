382775d2c502601988bcb79ce1c4884bd230cc0d
DanielGMelgarejo <145459680+DanielGMelgarejo@users.noreply.github.com>
2024-01-08 20:51:40 +0100

    update 7/01
    Op datum 7/01 werkte ik aan mijn om mogelijke fouten te verbteren in dit geval, zijn er fotos vervangen naar duidelijke fotot, aangezien dat sommige fotos het tekst niet leesbaar maakten (3 fotos vervagen) java script vervagen en oplossing gevonden aan probleem, namelijk kaarten waren ineens niet meer zichtbaar. Oorzaak van het probleem, "sint jans plein" het naam de pleintje moest juist geschreven zijn, dus "sint-jansplein"

--- SintJansplein.js
+++ SintJansplein.js
@@ -1,99 +1,89 @@
 
-
-
 let Veemarkt = L.map('Veemarkt').setView([51.22342024196948, 4.400164108514011], 17);
-
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-}).addTo(Veemarkt);
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'}).addTo(Veemarkt);
 
-let apMarker = L.marker([51.22342024196948, 4.400164108514011]).addTo(Veemarkt);
-apMarker.bindPopup("<b>Veemarkt 15 Antwerp 2000, Belgium</b>").openPopup();
+var circleCenterVeemarkt = [51.22342024196948, 4.400164108514011];var circleRadiusVeemarkt = 20; 
+
+L.circle(circleCenterVeemarkt, {color: "#e60005", weight: 1, radius: circleRadiusVeemarkt}).addTo(Veemarkt);
+let VeemarktMarker = L.marker([51.22342024196948, 4.400164108514011]).addTo(Veemarkt);VeemarktMarker.bindPopup("<b>Veemarkt 15 Antwerp 2000, Belgium</b>").openPopup();
 
 
 
-
-let SintJansplein = L.map('SintJansplein').setView([51.22553523110052, 4.4177753492286], 17);
-
+var SintJansvliet = L.map('SintJansvliet').setView([51.21861952192872, 4.396497402461873], 17);
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-}).addTo(SintJansplein);
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'}).addTo(SintJansvliet);
 
-let marker = L.marker([51.22553523110052, 4.4177753492286]).addTo(SintJansplein);
-marker.bindPopup("<b>Sint-Jansplein 17, Antwerp 2060, Belgium</b>").openPopup();
+var circleCenterSintJansvliet = [51.21861952192872, 4.396497402461873];var circleRadiusSintJansvliet = 20; 
 
+L.circle(circleCenterSintJansvliet, {color: "#e60005", weight: 1, radius: circleRadiusSintJansvliet}).addTo(SintJansvliet);
 
-
-
-let SintJansvliet = L.map('SintJansvliet').setView([51.21861952192872, 4.396497402461873], 17);
-
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-}).addTo(SintJansvliet);
-
-let Vmarkr = L.marker([51.21861952192872, 4.396497402461873]).addTo(SintJansvliet);
-Vmarkr.bindPopup("<b>Sint-Jansvliet 15, Antwerp 2000, Belgium<br>").openPopup();
+var SintJansvlietMarker = L.marker([51.21861952192872, 4.396497402461873]).addTo(SintJansvliet);SintJansvlietMarker.bindPopup("<b>Sint-Jansvliet 15, Antwerp 2000, Belgium</b>").openPopup();
 
 
-
-
-let Viséstraat = L.map('Viséstraat').setView([51.2284271520852, 4.429157392680716], 17);
+var Viséstraat = L.map('Viséstraat').setView([51.2284271520852, 4.429157392680716], 17);
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(Viséstraat);
 
-let Vimarkr = L.marker([51.2284271520852, 4.429157392680716]).addTo(Viséstraat);
-Vimarkr.bindPopup("<b>Viséstraat 90, Antwerp 2060, Belgium</b>").openPopup();
+        
+var circleCenterViséstraat = [51.2284271520852, 4.429157392680716];
+var circleRadiusViséstraat = 20; 
 
+        
+L.circle(circleCenterViséstraat, {color: "#e60005", weight: 1, radius: circleRadiusViséstraat}).addTo(Viséstraat);
 
+        
+var ViséstraatMarker = L.marker([51.2284271520852, 4.429157392680716]).addTo(Viséstraat);
+ViséstraatMarker.bindPopup("<b>Viséstraat 90, Antwerp 2060, Belgium</b>").openPopup();
 
-
-let Damplein = L.map('Damplein').setView([51.232249543501666, 4.425098663093153], 17);
+var Damplein = L.map('Damplein').setView([51.232249543501666, 4.425098663093153], 17);
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+     maxZoom: 19,
+     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(Damplein);
 
-let Dmarkr = L.marker([51.232249543501666, 4.425098663093153]).addTo(Damplein);
-Dmarkr.bindPopup("<b>Damplein 11, 2060 Antwerpen</b>").openPopup();
+        
+var circleCenterDamplein = [51.232249543501666, 4.425098663093153];
+var circleRadiusDamplein = 20; 
+
+   
+L.circle(circleCenterDamplein, {color: "#e60005", weight: 1, radius: circleRadiusDamplein}).addTo(Damplein);
+
+        
+var DampleinMarker = L.marker([51.232249543501666, 4.425098663093153]).addTo(Damplein);
+DampleinMarker.bindPopup("<b>Damplein 11, 2060 Antwerpen</b>").openPopup();
+
 
 
 var Lovelingstraat = L.map('Lovelingstraat').setView([51.21912596067735, 4.431694698937536], 17);
-
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-}).addTo(Lovelingstraat);
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'}).addTo(Lovelingstraat);
 
-// bepaal de cirkel rondom Lovelingstraat
-var circleCenter = [51.21912596067735, 4.431694698937536];
-var circleRadius = 20; // Pas dit aan op basis van je vereisten
+var circleCenter = [51.21912596067735, 4.431694698937536];var circleRadius = 20;
 
-// teken de cirkel in met de rode kleur
 L.circle(circleCenter, {color: "#e60005", weight: 1, radius: circleRadius}).addTo(Lovelingstraat);
 
-// plaats een marker evenwijdig aan de gegeven coördinaten met als tekst "Lovelingstraat 56, Antwerp 2060, Belgium"
-var parallelMarker = L.marker([51.21912596067735, 4.431694698937536]).addTo(Lovelingstraat);
-parallelMarker.bindPopup("<b>Lovelingstraat 56, Antwerp 2060, Belgium</b>").openPopup();
-  
+var parallelMarker = L.marker([51.21912596067735, 4.431694698937536]).addTo(Lovelingstraat);parallelMarker.bindPopup("<b>Lovelingstraat 56, Antwerp 2060, Belgium</b>").openPopup();
 
 
 
 
+var Sintjansplein = L.map('Sintjansplein').setView([51.225350650086554, 4.417644590919045], 17);
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'}).addTo(Sintjansplein);
 
+var circleCenterSintjansplein = [51.225350650086554, 4.417644590919045];var circleRadiusSintjansplein = 50; 
 
-
-
-
-
-
-
+L.circle(circleCenterSintjansplein, {color: "#e60005", weight: 1, radius: circleRadiusSintjansplein}).addTo(Sintjansplein);
+var SintjanspleinMarker = L.marker([51.225350650086554, 4.417644590919045]).addTo(Sintjansplein);SintjanspleinMarker.bindPopup("<b> Sint-jansplein 17, 2060 Belgium</b>").openPopup();
 
 
 
--- Veemarkt.js
+++ Veemarkt.js
@@ -1,18 +1,21 @@
-let ringlaan = L.map('ringlaan').setView([51.25404350251952, 4.456759275570227], 17);  
+
+  
+var ringlaan = L.map('ringlaan').setView([51.25404350251952, 4.456759275570227], 17);
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(ringlaan);
 
-let ringlaan36 = L.marker([51.25404350251952, 4.456759275570227]).addTo(ringlaan);
-ringlaan36.bindPopup("<b>Ringlaan36, 2170 Antwerpen</b>").openPopup();
+// Coördinaten voor de cirkel
+var circleCenterRinglaan = [51.25404350251952, 4.456759275570227];
+var circleRadiusRinglaan = 10; // Pas dit aan op basis van je vereisten
+
+// Teken de cirkel in met de rode kleur
+L.circle(circleCenterRinglaan, {color: "#e60005", weight: 1, radius: circleRadiusRinglaan}).addTo(ringlaan);
+
+// Marker voor Ringlaan36
+var ringlaan36Marker = L.marker([51.25404350251952, 4.456759275570227]).addTo(ringlaan);
+ringlaan36Marker.bindPopup("<b>Ringlaan36, 2170 Antwerpen</b>").openPopup();
 
 
-
-
-
-
-
-
-  
